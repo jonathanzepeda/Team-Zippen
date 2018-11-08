56c146481aa665bfa3868563dba6667c9737b405
jonathanzepeda <jaydotzee@mail.fresnostate.edu>
2018-11-07 16:05:21 -0800

    changed table back to app

--- src/Table/app.module.ts
+++ src/Table/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './table.component';
+import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 
 import { FormsModule} from '@angular/forms';
